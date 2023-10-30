1b47f92b71c08efe19c8e4faf5a1eac9927e6b90
Github-user191 <luqmaan786921@gmail.com>
2023-10-30 12:18:04 +0200

    fixed cors config

--- backend/src/main/java/com/carspot/app/config/SecurityConfig.java
+++ backend/src/main/java/com/carspot/app/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -70,8 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable().formLogin().disable().httpBasic().disable();
-
+            .cors(Customizer.withDefaults()).csrf().disable().formLogin().disable().httpBasic().disable();
         http
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
